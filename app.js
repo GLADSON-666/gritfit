46a59a83015e0ae3bc38a3b269551912d8f9054f
GLADSON-666 <gladsongeorge686@gmail.com>
2025-09-01 16:52:03 +0530

    signup api completed

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+const Express = require("express")
+const cors = require("cors")
+const Mongoose = require("mongoose")
+const Bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
-const Express=require("express")
-const cors=require("cors")
-const Mongoose=require("mongoose")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
+let app = Express()
+app.use(Express.json())
+app.use(cors())
 
-let app=Express()
+Mongoose.connect("mongodb+srv://GladsonGeorge:chimera@cluster0.c89d4qt.mongodb.net/gritfit?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.post("/signup", async (req, res) => {
+    try {
+        let input = req.body
+
+        // hash password
+        let hashedpassword = Bcrypt.hashSync(req.body.password, 10)
+        input.password = hashedpassword
+
+        // check if email exists
+        let check = await userModel.findOne({ email: req.body.email })
+        if (check) {
+            return res.json({ status: "email id already exist" })
+        }
+
+        // save new user
+        let result = new userModel(input)
+        await result.save()
+
+        res.json({ status: "success" })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ status: "error", message: "Something went wrong" })
+    }
 })
-app.listen(3030,()=>{
+
+app.listen(3030, () => {
     console.log("server started")
 })
